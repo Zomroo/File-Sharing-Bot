a4c28e517a57c5cbb61353934fb44dd6257dc79a
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-28 22:10:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -72,7 +72,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(541388244)
+ADMINS.append(5500572462)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
